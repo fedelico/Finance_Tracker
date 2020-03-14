a1c564a49cfea10db8116b744ed26e96488e1cde
fedelico <u105000017@cmu.edu.tw>
2020-03-15 00:09:46 +0800

    created main windows of the app

--- finance_tracker.py
+++ finance_tracker.py
@@ -1,4 +1,26 @@
 import tkinter as tk
 
+class Finance_Tracker_App(tk.Tk):
+    def __init__(self, *args, **kwargs):
+        super().__init__(None, *args, **kwargs)
+        self.title("FinanceTracker")
+        self.geometry("700x350")
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app = Finance_Tracker_App()
+app.mainloop()
