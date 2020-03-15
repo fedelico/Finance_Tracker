a48a680b999ab66dbdc16e087f98884ae800de96
fedelico <u105000017@cmu.edu.tw>
2020-03-15 12:57:59 +0800

    built multi-page interface

--- finance_tracker.py
+++ finance_tracker.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-
+from tkinter import ttk
 class Finance_Tracker_App(tk.Tk):
     def __init__(self):
         super().__init__()
@@ -7,21 +7,49 @@ class Finance_Tracker_App(tk.Tk):
         self.geometry("700x350")
 
         # initializes pages
-        self.pages = {Home_Page, main_Page, Plot_Page}
+        self.pages = {'Home_Page':Home_Page, 'Main_Page':Main_Page, 'Plot_Page':Plot_Page}
+        for name, P in self.pages.items():
+            self.pages[name] = P(self)
+            self.pages[name].grid(row = 0, column = 0, sticky = 'nsew')
 
+        self.show_page("Home_Page")
+
+    def show_page(self, page):
+        self.pages[page].tkraise()
+
+class Home_Page(tk.Frame):
+    def __init__(self, container):
+        super().__init__(container)
+        l1 = ttk.Label(self, text= "This is Home_Page")
+        go_main_page = ttk.Button(self, text = "To Main_Page", command = lambda: container.show_page("Main_Page"))
+        go_plot_page = ttk.Button(self, text = "To Plot_Page", command = lambda: container.show_page("Plot_Page"))
         
+        l1.grid(row = 0, column = 2, sticky = 'n')
+        go_main_page.grid(row = 3, column = 1, sticky = 's')
+        go_plot_page.grid(row = 3, column = 3, sticky = 's')
 
 
+class Main_Page(tk.Frame):
+    def __init__(self, container):
+        super().__init__(container)
+        l1 = ttk.Label(self, text= "This is Main_Page")
+        go_home_page = ttk.Button(self, text = "To Home_Page", command =lambda: container.show_page("Home_Page"))
+        go_plot_page = ttk.Button(self, text = "To Plot_Page", command =lambda: container.show_page("Plot_Page"))
+        
+        l1.grid(row = 0, column = 2, sticky = 'n')
+        go_home_page.grid(row = 3, column = 1, sticky = 's')
+        go_plot_page.grid(row = 3, column = 3, sticky = 's')
 
-
-
-
-
-
-
-
-
-
+class Plot_Page(tk.Frame):
+    def __init__(self, container):
+        super().__init__(container)
+        l1 = ttk.Label(self, text= "This is Plot_Page")
+        go_home_page = ttk.Button(self, text = "To Home_Page", command = lambda: container.show_page("Home_Page"))
+        go_main_page = ttk.Button(self, text = "To Main_Page", command = lambda: container.show_page("Main_Page"))
+        
+        l1.grid(row = 0, column = 2, sticky = 'n')
+        go_main_page.grid(row = 3, column = 1, sticky = 's')
+        go_home_page.grid(row = 3, column = 3, sticky = 's')
 
 
 
