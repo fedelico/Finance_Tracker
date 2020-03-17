86414d48be78ab3b1e34a4f04c2b74d968ba225d
fedelico <u105000017@cmu.edu.tw>
2020-03-17 23:27:49 +0800

    completed sketch version of Data_base class

--- data_management.py
+++ data_management.py
@@ -1,33 +1,47 @@
 import sqlite3
+import datetime
 
 class Data_base(object):
     def __init__(self, name):
         """ initialize a database """
-        self.record_name = record_name
+        self.record_name = name
         self.connection = sqlite3.connect(f"{self.record_name}.db")
         self.cursor = self.connection.cursor()
-        
-    def create_table(self, name, properties):
-        """
-        create a table in your data base
-        param:
-            self
-            name(str) -> table name
-            properties(dict) -> properties that you want to set your column with ex: {firstName: text, lastName: text, pay: integer}
-        """
-        if type(name) == str and type(properties) == dict:
-            self.cursor.execute("""CREATE TABLE :name( )""")
-        else:
-            raise TypeError("wrong data type passed in the function")
-    
-    def insert_data(self, table_name, values):
-        pass
+        self.cursor.execute("""CREATE TABLE IF NOT EXISTS balance_sheet (id integer PRIMARY KEY NOT NULL AUTOINCREMENT
+                                                                         user_name text
+                                                                         expense integer
+                                                                         revenue integer
+                                                                         time text
+                                                                        )""")
+        sef.cursor.execute("""CREATE TABLE IF NOT EXISTS users (uid integer NOT NULL AUTO_INCREMENT
+                                                  user_name text
+                                                  pass_word_hash text
+                                                  balance integer)""")
+        self.connection.commit()
 
-    def delete_data(self, table_name, values):
-        pass
+
+    def update_data(self, column_name, value):
+        self.cursor.execute("UPDATE users SET ? = ?", (column_name, value))
+        self.connection.commit()
+
+
+    def insert_data(self, table_name, values):
+        if table_name == "balance_sheet":
+            self.cursor.execute("INSERT INTO balance_sheet VALUES (?, ?, ?, ?)", (user_name, values[0], values[1], str(datetime.datetime.today)))# todo: find a way to put user name in it
+            self.connection.commit()
+
+        elif table_name == "users":
+            self.cursor.execute("INSERT INTO users VALUES (?, ?, ?)", (values[0], values[1], 0))
+            self.connection.commit()
+
+        else:
+            raise ValueError("table name does not exist")
+
+    def delete_data(self, value):
+        self.cursor.execute("DELETE FROM balance_sheet WHERE id = ?", (value,))
+        self.connection.commit()
 
     def get_data(self, table_name, criteria):
         pass
     
-    def 
 
