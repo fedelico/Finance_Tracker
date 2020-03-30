a94fd613aa5035e37295a9089b22ab87d0dae5ce
fedelico <u105000017@cmu.edu.tw>
2020-03-31 02:07:39 +0800

    Implemented Sign_up_Page class

--- finance_tracker.py
+++ finance_tracker.py
@@ -35,6 +35,55 @@ class Finance_Tracker_App(tk.Tk):
     def show_page(self, page):
         self.pages[page].tkraise()
 
+class Sign_up_Page(tk.Frame):
+    def __init__(self, container):
+        self.container = container
+        self.name, self.password, self.password_check = tk.StringVar(), tk.StringVar(), tk.StringVar()
+        super().__init__(container)
+        l2 = ttk.Label(text = "This is Sign_up_Page")
+        go_login = ttk.Button(text = "Back to login", command = lambda: container.show_page("Login_Page"))
+        user_name = ttk.Label(self, text = "USER NAME", font = ("bold", 14))
+        self.name_entry = Labeled_Entry(self, default_text = "Enter your user name here", textvariable = name)
+       
+        pass_word = ttk.Label(self, text = "PASS WORD", font = ("bold", 14))
+        self.pswd_entry = Labeled_Entry(self, default_text = "Enter your password here", textvariable = password, show = '*')
+        
+        pass_word_confirmation = ttk.Label(self, text = "CONFIRM YOUR PASS WORD")
+        self.pass_word_confirm_entry = Labeled_Entry(self, default_text = "Enter your password again", textvariable = password_check, show = '*')
+        
+        send = ttk.Button(self, text = "login", command = lambda: add_new_user(self))
+
+        l2.grid(row = 0, column = 0, columnspan = 4, sticky = 'we')
+        go_login.grid(row = 5, column = 1)
+        user_name.grid(row = 1, column = 2)
+        self.name_entry.grid(row = 1, column = 3)
+        pass_word.grid(row = 2, column = 2)
+        self.pswd_entry.grid(row = 2, column = 3)
+        pass_word_confirmation.grid(row = 3, column = 2)
+        self.pass_word_confirm_entry.grid(row = 3, column = 3)
+        send.grid(row = 4, column = 3)
+        #make buttons resizable with the page(frame)
+        self.grid_rowconfigure(1, weight = 1)
+        self.grid_rowconfigure(2, weight = 1)
+        self.grid_rowconfigure(3, weight = 1)
+        self.grid_columnconfigure(2, weight = 1)
+        self.grid_columnconfigure(3, weight = 1)
+    
+    def add_new_user(self):
+        self.name_entry.get()
+        self.pswd_entry.get()
+        self.password_confirm_entry.get()
+        if self.name == '' or self.password == '' or self.password_check == '':
+            pass
+        if self.password != self.password_check:
+            pass
+        self.container.db.cursor.execute("SELECT * from users where user_name = ?", (self.name))
+        if self.container.db.cursor.fetchone() == None:
+            pass
+        
+        self.container.db.insert_data("users", (self.name, self.password, 0))
+
+
 class Login_Page(tk.Frame):
     def __init__(self, container):
         super().__init__(container)
